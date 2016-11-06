b14191dd44e6a17b60f5c42b521b754ddcdd7954
Chris Dannen <chris.dannen@gmail.com>
2016-11-05 22:45:01 -0400

    Create owned.sol

---
+++ owned.sol
@@ -0,0 +1,13 @@
+//! Owned contract.
+//! By Gav Wood (Ethcore), 2016.
+//! Released under the Apache Licence 2.
+
+contract Owned {
+    modifier only_owner { if (msg.sender != owner) return; _ }
+    
+    event NewOwner(address indexed old, address indexed current);
+    
+    function setOwner(address _new) only_owner { NewOwner(owner, _new); owner = _new; }
+    
+    address public owner = msg.sender;
+}
