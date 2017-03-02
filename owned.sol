262dccd2f661c0c0dc985e74ecee5d0e3972b5f2
Chris Dannen <chris.dannen@gmail.com>
2017-03-02 14:14:41 -0500

    Update owned.sol

--- owned.sol
+++ owned.sol
@@ -2,14 +2,14 @@
 //! By Gav Wood (Ethcore), 2016.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.6;
 
 contract Owned {
-    modifier only_owner { if (msg.sender != owner) return; _ }
-    
+    modifier only_owner { if (msg.sender != owner) return; _; }
+
     event NewOwner(address indexed old, address indexed current);
-    
+
     function setOwner(address _new) only_owner { NewOwner(owner, _new); owner = _new; }
-    
+
     address public owner = msg.sender;
 }
