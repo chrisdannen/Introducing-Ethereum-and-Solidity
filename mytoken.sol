f9f9ee5d884d050a968b609c8733530af3e97aaf
Chris Dannen <chris@iterative.capital>
2018-10-09 09:04:13 -0400

    Updating name of constructor function
    Defining constructors as functions with the same name as the contract is deprecated.

--- mytoken.sol
+++ mytoken.sol
@@ -12,7 +12,7 @@ contract MyToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MyToken(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
+    constructor(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
         /* if supply not given then generate 1 million of the smallest unit of the token */
         if (_supply == 0) _supply = 1000000;
 
